85e4b71bbc646b1bf2b0dd6bcdd5b4e1641bce95
pea <pea040119@gmail.com>
2024-10-05 01:38:49 +0900

    test

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,25 +1,22 @@
 use ansi_term::Colour;
+use crate::utils::constants::{error_header, success_header};
 
 pub mod pea_test;
 
 
 
 pub fn test(tester: Option<&String>){
-    let error_header: ansi_term::ANSIGenericString<'_, str> = Colour::Red.paint("[!] Error");
-    let success_header: ansi_term::ANSIGenericString<'_, str> = Colour::Green.paint("[+] Success");
-    
-
     match tester {
         Some(tester) => {
             let tester_print: ansi_term::ANSIGenericString<'_, str> = Colour::Blue.paint(tester.as_str());
             match tester.as_str() {
                 "pea" => {
-                    println!("{}: Running the test for {} tester...", success_header, tester_print);
+                    println!("{}: Running the test for {} tester...", success_header(), tester_print);
                     pea_test::test();
-                    println!("{}: {} test completed successfully.", success_header, tester_print);
+                    println!("{}: {} test completed successfully.", success_header(), tester_print);
                 },
                 _ => {
-                    eprintln!("{}: {} is not listed as a recognized tester.", error_header, tester_print);
+                    eprintln!("{}: {} is not listed as a recognized tester.", error_header(), tester_print);
                 }
             }
         },
