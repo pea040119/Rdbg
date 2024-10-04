7adacbc46ac1b79461b0125350c779c47bb0e5ad
pea <pea040119@gmail.com>
2024-10-04 14:24:49 +0900

    Make test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use ptracer::Ptracer;
-use std::process::Command;
-use nix::unistd::Pid;
+use clap::{Command, Arg};
+
+mod test;
 
 
 
 fn main() {
-    println!("Hello, world!");
+    let args: clap::ArgMatches = Command::new("Rgdb")
+        .arg(
+            Arg::new("test")
+                .long("test")
+                .short('t')
+                .help("Input Tester Name")
+                .num_args(1)
+        )
+        .get_matches();
+    
+    test::test(args.get_one::<String>("test"));
 }
-
-
-
-fn
