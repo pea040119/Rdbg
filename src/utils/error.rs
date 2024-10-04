909f4e20fa1cf7ef4f07c30b6be955f243bdf2d5
pea <pea040119@gmail.com>
2024-10-05 01:39:04 +0900

    error

---
+++ src/utils/error.rs
@@ -0,0 +1,39 @@
+use std::fmt;
+use std::error::Error;
+use crate::utils::constants::{error_header, success_header};
+
+
+
+pub struct DbgError {
+    msg: String,
+}
+
+
+impl DbgError {
+    pub fn new(msg: &str) -> Self {
+        return DbgError {
+           msg: msg.to_string(),
+       }
+    }
+}
+
+
+impl fmt::Display for DbgError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        return write!(f, "{}: {}", error_header(), self.msg);
+    }
+}
+
+
+impl fmt::Debug for DbgError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}: {}", error_header(), self.msg)
+    }
+}
+
+
+impl Error for DbgError {
+    fn description(&self) -> &str {
+        return &self.msg;
+    }
+}
