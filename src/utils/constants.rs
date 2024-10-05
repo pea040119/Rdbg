9cb08f980e02ba6cb58cb822e5c930524540913b
pea <pea040119@gmail.com>
2024-10-05 22:58:44 +0900

    add commands, options

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -10,3 +10,13 @@ pub fn error_header() -> ansi_term::ANSIGenericString<'static, str> {
 pub fn success_header() -> ansi_term::ANSIGenericString<'static, str> {
     return Colour::Green.paint("[+] Success");
 }
+
+
+pub fn commands() -> ansi_term::ANSIGenericString<'static, str> {
+    return Colour::Green.paint("Command:");
+}
+
+
+pub fn options() -> ansi_term::ANSIGenericString<'static, str> {
+    return Colour::Cyan.paint("Options:");
+}
